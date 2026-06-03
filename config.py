4cfc9470d995369baef0e04410ff8bc513bf57c0
Attar Mohammad Awais <158297692+Awais-attar@users.noreply.github.com>
2026-06-03 19:44:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,7 @@ class Config:
 
 SECRET_KEY = "student_secret_key"
 
-SQLALCHEMY_DATABASE_URI = (
-    "mysql+pymysql://root:1111@localhost/student_management"
-)
+SQLALCHEMY_DATABASE_URI = "sqlite:///school.db"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
